c4b6e984f262f2a702e6678daebd5dc618ae03c2
chriskrieg <chriskrieg95@gmail.com>
2019-12-03 13:47:06 -0500

    first commit for RecipeApp

---
+++ src/test/java/co/grandcircus/RecipeApp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.RecipeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
